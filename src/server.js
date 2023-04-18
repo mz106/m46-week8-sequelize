e56d1d8ddc736c451c35e96dd549dcb8d8ed9aaf
Michael Z <michaelzwierzanski@gmail.com>
2023-04-18 13:55:59 +0100

    add express.json

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
   res.status(200).json({ message: "App is healthy" });
 });
