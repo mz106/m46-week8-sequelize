03b07ebf5edd0581d553fb1ffd820ac9db84628c
Michael Z <michaelzwierzanski@gmail.com>
2023-04-18 14:41:20 +0100

    add db connection

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 
+const port = process.env.PORT || 5001;
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,6 @@ app.get("/health", (req, res) => {
   res.status(200).json({ message: "App is healthy" });
 });
 
-app.listen(5001, () => {
+app.listen(port, () => {
   console.log("Server is listening");
 });
